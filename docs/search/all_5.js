1fab453c1185abf6c331ebc4ec9294673ab675e5
LoRd_MuldeR <mulder2@gmx.de>
2016-12-25 22:16:12 +0100

    Regenerated documents.

--- docs/search/all_5.js
+++ docs/search/all_5.js
@@ -3,12 +3,15 @@ var searchData=
   ['family',['family',['../dd/de2/struct_m_utils_1_1_c_p_u_fetaures_1_1__cpu__info__t.html#afefb6e7d6165b8de9bee1f17df826392',1,'MUtils::CPUFetaures::_cpu_info_t']]],
   ['features',['features',['../dd/de2/struct_m_utils_1_1_c_p_u_fetaures_1_1__cpu__info__t.html#a8f312e3386a9f56a8d10b890af3ce3af',1,'MUtils::CPUFetaures::_cpu_info_t']]],
   ['flag_5favx',['FLAG_AVX',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#ad8a4de23ab21ecd4871358e7edea90cb',1,'MUtils::CPUFetaures']]],
+  ['flag_5favx2',['FLAG_AVX2',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a9d8a032c0fdeb501e4a39826ab2aa23c',1,'MUtils::CPUFetaures']]],
   ['flag_5fcmov',['FLAG_CMOV',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#adf2416d7ed2553f313fb08c289365f5c',1,'MUtils::CPUFetaures']]],
+  ['flag_5ffma3',['FLAG_FMA3',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a0700ba3ad4c671b7a6465b871bd60d2a',1,'MUtils::CPUFetaures']]],
+  ['flag_5flzcnt',['FLAG_LZCNT',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#acac041ff2292d32c1bd3f26f61486a01',1,'MUtils::CPUFetaures']]],
   ['flag_5fmmx',['FLAG_MMX',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a1c2235ceac6576dace2a0ea9ddc1cbdf',1,'MUtils::CPUFetaures']]],
   ['flag_5fsse',['FLAG_SSE',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a02a1dd90366db8200e38299635e54a90',1,'MUtils::CPUFetaures']]],
   ['flag_5fsse2',['FLAG_SSE2',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a9ba4833265029796db9279e9bd8beae8',1,'MUtils::CPUFetaures']]],
   ['flag_5fsse3',['FLAG_SSE3',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a52bb099e73cb9f6d02ef6ac9f89b4061',1,'MUtils::CPUFetaures']]],
-  ['flag_5fsse4',['FLAG_SSE4',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#af9c23f847bf250fe26ee353b57f8db3f',1,'MUtils::CPUFetaures']]],
+  ['flag_5fsse41',['FLAG_SSE41',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a425bc8f89d64bf6dafdbd00c89b9874b',1,'MUtils::CPUFetaures']]],
   ['flag_5fsse42',['FLAG_SSE42',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#afcb03b5edb1518bfca9984ff9ccc4f99',1,'MUtils::CPUFetaures']]],
   ['flag_5fssse3',['FLAG_SSSE3',['../d3/d02/namespace_m_utils_1_1_c_p_u_fetaures.html#a5125a29c37e57656f45ad168e654fd05',1,'MUtils::CPUFetaures']]]
 ];
