7af7321266e40aa313a9b9ca0fbfec64345f4dbc
LoRd_MuldeR <mulder2@gmx.de>
2019-07-14 15:23:52 +0200

    Added support for Visual Studio 2017.9 (v15.9.14).

--- include/MUtils/Version.h
+++ include/MUtils/Version.h
@@ -80,7 +80,7 @@ namespace MUtils
 							#error Compiler version is not supported yet!
 						#endif
 					#elif (_MSC_VER == 1916)
-						#if((_MSC_FULL_VER >= 191627024) && (_MSC_FULL_VER <= 191627031))
+						#if((_MSC_FULL_VER >= 191627024) && (_MSC_FULL_VER <= 191627032))
 							"MSVC 2017.9";
 						#else
 							#error Compiler version is not supported yet!
