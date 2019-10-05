edc89121f9ba3b4ed174bfe2ff4df1520c11fab7
LoRd_MuldeR <mulder2@gmx.de>
2019-10-05 17:56:54 +0200

    Added support for Visual Studio 2019.3 (v16.3).

--- include/MUtils/Version.h
+++ include/MUtils/Version.h
@@ -67,7 +67,13 @@ namespace MUtils
 						#error Compiler is not supported!
 					#endif
 				#elif defined(_MSC_VER)
-					#if (_MSC_VER == 1922)
+					#if (_MSC_VER == 1923)
+						#if((_MSC_FULL_VER >= 192328105) && (_MSC_FULL_VER <= 192328105))
+							"MSVC 2019.3";
+						#else
+							#error Compiler version is not supported yet!
+						#endif
+					#elif (_MSC_VER == 1922)
 						#if((_MSC_FULL_VER >= 192227905) && (_MSC_FULL_VER <= 192227905))
 							"MSVC 2019.2";
 						#else
