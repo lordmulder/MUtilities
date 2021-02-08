bb4290749362d186a736b5428dcb5c0aef173799
LoRd_MuldeR <mulder2@gmx.de>
2021-02-08 20:59:25 +0100

    Updated copyright year.

--- src/JobObject_Win32.cpp
+++ src/JobObject_Win32.cpp
@@ -1,6 +1,6 @@
 ///////////////////////////////////////////////////////////////////////////////
 // MuldeR's Utilities for Qt
-// Copyright (C) 2004-2019 LoRd_MuldeR <MuldeR2@GMX.de>
+// Copyright (C) 2004-2021 LoRd_MuldeR <MuldeR2@GMX.de>
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
